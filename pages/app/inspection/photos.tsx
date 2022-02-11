e0175490a2f169f8a7086f06dc13dbe6fe43ea71
Thiago Gois <goislimat2@gmail.com>
2022-02-11 16:09:57 -0300

    Finish photos screen

--- pages/app/inspection/photos.tsx
+++ pages/app/inspection/photos.tsx
@@ -1,5 +1,33 @@
+import Link from "next/link";
+import styled from "styled-components";
+
+const Screen = styled.a`
+  position: absolute;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  background: #24263b;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  text-decoration: none;
+
+  span {
+    color: white;
+    font-size: 28px;
+    line-height: 34px;
+  }
+`;
+
 const Photos = () => {
-  return <div>big black screen</div>;
+  return (
+    <Link href="/app/contract" passHref>
+      <Screen>
+        <span>Fotos</span>
+      </Screen>
+    </Link>
+  );
 };
 
 export default Photos;
