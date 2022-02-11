157b36bb82eea0bb2e56e569bede29e1ed52af29
Thiago Gois <goislimat2@gmail.com>
2022-02-11 16:05:29 -0300

    Finish inspection screen

--- pages/app/inspection/index.tsx
+++ pages/app/inspection/index.tsx
@@ -1,9 +1,20 @@
+import Image from "next/image";
 import { Button, Screen } from "../../../src/ui";
 
 const Inspection = () => {
   return (
     <Screen previousPage="/app/contract" title="Vamos iniciar a vistoria?">
-      <p>Aqui a gente mostra as instruções da vistoria</p>
+      <Image
+        src="/assets/inspection_car.png"
+        alt="inpection car"
+        height={81}
+        width={107}
+      />
+      <h2>Hora da vistoria!</h2>
+      <p>
+        A vistoria Pier é nosso jeito de analisar se conseguimos proteger seu
+        carro. Separe 10 minutinhos do seu dia e prepare-se:
+      </p>
       <Button nextPage="inspection/photos">Tirar fotos</Button>
     </Screen>
   );
