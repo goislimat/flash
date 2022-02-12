6a08311310055dd4c790f2049e4ed01bafc61705
Thiago Gois <goislimat2@gmail.com>
2022-02-12 17:35:41 -0300

    Improve on link tree

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,21 @@
 import type { NextPage } from "next";
 import Link from "next/link";
+import styled from "styled-components";
 
 import { Screen } from "../src/ui";
+
+const List = styled.ul`
+  li {
+    font-size: 18px;
+    line-height: 24px;
+    margin-bottom: 16px;
+  }
+`;
 
 const Home: NextPage = () => {
   return (
     <Screen title="Link Tree">
-      <ul>
+      <List>
         <li>
           <Link href="partner/login">PARTNER</Link>
         </li>
@@ -16,7 +25,7 @@ const Home: NextPage = () => {
         <li>
           <Link href="auth/login">APP</Link>
         </li>
-      </ul>
+      </List>
     </Screen>
   );
 };
